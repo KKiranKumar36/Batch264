2edf348c1d3271a1f37bb6cd177a5c5c27bb94c6
BhagyaDhathriSaiSri <sri.bhagi66@gmail.com>
2023-08-15 01:29:41 +0530

    my Test2 first commit

---
+++ src/test/java/javaprograms/Test2.java
@@ -0,0 +1,14 @@
+package javaprograms;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+	public void method2()
+	{
+		Reporter.log("Hello test Automation Devs");
+	}
+
+}
